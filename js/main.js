fae34cad53640e6f44a5a8a3bd4811bcc93abdb7
hannah curley <105502750+hannahcurley@users.noreply.github.com>
2022-08-28 22:24:54 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
+function myFunction() {
+  var x = document.getElementById("mobilebtns");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  
-  /* Close */
-  function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-  }
+}
